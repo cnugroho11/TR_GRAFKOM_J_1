041a4576148b10c113b9fe8c073bb2e401525513
cnugroho211 <cnugroho211@gmail.com>
2020-07-10 12:32:57 +0700

    starting code

--- main.cpp
+++ main.cpp
@@ -1,177 +1,266 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
 #include <windows.h>
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
 #include <GL/glut.h>
-#endif
-
 #include <stdlib.h>
 
-static int slices = 16;
-static int stacks = 16;
+void init(void);
+void tampil(void);
+void keyboard(unsigned char, int, int);
+void mouse(int button, int state, int x, int y);
+void ukuran(int,int);
+void mouseMotion(int x, int y);
 
-/* GLUT callback Handlers */
+float xrot = 0.0f;
+float yrot = 0.0f;
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+bool mouseDown = false;
+int is_depth;
 
-static void resize(int width, int height)
-{
-    const float ar = (float) width / (float) height;
-
-    glViewport(0, 0, width, height);
+void init(void){
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
-
+    glEnable(GL_DEPTH_TEST);
+    is_depth = 1;
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+    glPointSize(9.0);
+    glLineWidth(6.0);
+
 }
 
-static void display(void)
+int main(int argc,char **argv)
 {
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800,600);
+    glutInitWindowPosition(250,80);
+    glutCreateWindow("Cahyo Nugroho - 672018039");
+    init();
+    glutDisplayFunc(tampil);
+    glutKeyboardFunc(keyboard);
+    glutReshapeFunc(ukuran);
+    glutMouseFunc(mouse);
+    glutMotionFunc(mouseMotion);
+    glutIdleFunc(tampil);
+    glutMainLoop();
+    return 0;
+}
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
 
+void tembok(){
+
+    glColor3f(1.0,0.65,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(-20.0,-10.0,10.0);
+        glVertex3f(-20.0,20.0,10.0);
+        glVertex3f(10.0,20.0,10.0);
+        glVertex3f(10.0,-10.0,10.0);
+    glEnd();
+
+    glColor3f(1.0,0.1,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(10.0,-10.0,30.0);
+        glVertex3f(10.0,20.0,30.0);
+        glVertex3f(40.0,20.0,30.0);
+        glVertex3f(40.0,-10.0,30.0);
+    glEnd();
+
+    glColor3f(0.2,0.1,0.2);
+    glBegin(GL_QUADS);
+        glVertex3f(40.0,20.0,30.0);
+        glVertex3f(40.0,-10.0,30.0);
+        glVertex3f(40.0,-10.0,-100.0);
+        glVertex3f(40.0,20.0,-100.0);
+    glEnd();
+
+    glColor3f(1.0,0.1,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(-20.0,-10.0,-100.0);
+        glVertex3f(-20.0,20.0,-100.0);
+        glVertex3f(40.0,20.0,-100.0);
+        glVertex3f(40.0,-10.0,-100.0);
+    glEnd();
+
+    glColor3f(1.0,0.65,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(10.0,20.0,30.0);
+        glVertex3f(10.0,-10.0,30.0);
+        glVertex3f(10.0,-10.0,10.0);
+        glVertex3f(10.0,20.0,10.0);
+    glEnd();
+
+    glColor3f(1.0,0.2,1.0);
+    glBegin(GL_QUADS);
+        glVertex3f(-20.0,20.0,10.0);
+        glVertex3f(-20.0,-10.0,10.0);
+        glVertex3f(-20.0,-10.0,-100.0);
+        glVertex3f(-20.0,20.0,-100.0);
+    glEnd();
+
+    //sekat
+    glColor3f(1.0,0.65,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(-20.0,-10.0,-20.0);
+        glVertex3f(-20.0,20.0,-20.0);
+        glVertex3f(40.0,20.0,-20.0);
+        glVertex3f(40.0,-10.0,-20.0);
+    glEnd();
+}
+
+void atap(){
+    //miringkanan
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_QUADS);
+        glVertex3f(10.0,30.0,35.0);
+        glVertex3f(45.0,20.0,35.0);
+        glVertex3f(45.0,20.0,-105.0);
+        glVertex3f(10.0,30.0,-105.0);
+    glEnd();
+    //miringkiri
+    glBegin(GL_QUADS);
+        glVertex3f(10.0,30.0,35.0);
+        glVertex3f(-25.0,20.0,35.0);
+        glVertex3f(-25.0,20.0,-105.0);
+        glVertex3f(10.0,30.0,-105.0);
+    glEnd();
+    //tutupbawah
+    glColor3f(1.0,1.0,0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(45.0,20.0,35.0);
+        glVertex3f(-25.0,20.0,35.0);
+        glVertex3f(-25.0,20.0,-105.0);
+        glVertex3f(45.0,20.0,-105.0);
+    glEnd();
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(45.0,20.0,35.0);
+        glVertex3f(-25.0,20.0,35.0);
+        glVertex3f(10.0,30.0,35.0);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+        glVertex3f(45.0,20.0,-105.0);
+        glVertex3f(-25.0,20.0,-105.0);
+        glVertex3f(10.0,30.0,-105.0);
+    glEnd();
+
+}
+
+void tampil(void)
+{
     glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
 
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
+    if(is_depth){
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    }else{
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
+    gluLookAt(0.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    glRotatef(xrot, 1.0f, 0.0f,0.0f);
+    glRotatef(yrot, 0.0f, 1.0f, 0.0f);
 
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
+    tembok();
+    //atap();
 
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
 
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
     glPopMatrix();
-
     glutSwapBuffers();
 }
 
 
-static void key(unsigned char key, int x, int y)
+void idle(){
+    if(!mouseDown){
+        xrot+=0.3f;
+        yrot+=0.4f;
+    }
+glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y){
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+        mouseDown=true;
+        xdiff = x - yrot;
+        ydiff = -y + xrot;
+    }
+    else mouseDown = false;
+}
+
+void mouseMotion(int x, int y){
+    if(mouseDown){
+        yrot = x - xdiff;
+        xrot = y + ydiff;
+
+        glutPostRedisplay();
+    }
+}
+
+void keyboard(unsigned char key, int x,int y)
 {
     switch (key)
     {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+    case 'w':
+    case 'W':
+        glTranslatef(0.0,0.0,3.0);
+        break;
+    case 'd':
+    case 'D':
+        glTranslatef(3.0,0.0,0.0);
+        break;
+    case 's':
+    case 'S':
+        glTranslatef(0.0,0.0,-3.0);
+        break;
+    case 'a':
+    case 'A':
+        glTranslatef(-3.0,0.0,0.0);
+        break;
+    case '7':
+        glTranslatef(0.0,3.0,0.0);
+        break;
+    case '9':
+        glTranslatef(0.0,-3.0,0.0);
+        break;
+    case 'k':
+    case 'K':
+        glRotatef(2.0,1.0,0.0,0.0);
+        break;
+    case 'i':
+    case 'I':
+        glRotatef(-2.0,1.0,0.0,0.0);
+        break;
+    case 'l':
+    case 'L':
+        glRotatef(2.0,0.0,1.0,0.0);
+        break;
+    case 'j':
+    case 'J':
+        glRotatef(-2.0,0.0,1.0,0.0);
+        break;
+    case '1':
+        glRotatef(2.0,0.0,0.0,1.0);
+        break;
+    case '3':
+        glRotatef(-2.0,0.0,0.0,1.0);
+        break;
+    case '5':
+        if (is_depth){
+            is_depth = 0;
+            glDisable(GL_DEPTH_TEST);
+        }else{
+            is_depth = 1;
+            glEnable(GL_DEPTH_TEST);
+        }
     }
-
-    glutPostRedisplay();
+    tampil();
 }
 
-static void idle(void)
+void ukuran(int lebar, int tinggi)
 {
-    glutPostRedisplay();
-}
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
-/* Program entry point */
-
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
-    glutCreateWindow("GLUT Shapes");
-
-    glutReshapeFunc(resize);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
-    glutMainLoop();
-
-    return EXIT_SUCCESS;
+    if (tinggi==0){
+        tinggi = 1;
+    }
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(50.0,lebar/tinggi,5.0,500.0);
+    glTranslatef(0.0,-5.0,-150.0);
+    glMatrixMode(GL_MODELVIEW);
 }
